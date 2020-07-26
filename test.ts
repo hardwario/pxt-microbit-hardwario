9482218e69bb67877c43c79cd87b732e3481d5a3
Jakub Smejkal <smejkal.jakub99@gmail.com>
2020-07-26 21:40:12 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,15 +1,20 @@
 /**
- * Set Relay Module bi-stable relay state on button press
+ * Set Relay Module bi-stable relay and power module Relay state on button press
+ * SUCCESS - relay switches on the button press
  */
 input.onButtonPressed(Button.B, function () {
+    hardwario.setPowerModuleRelay(RelayState.Off)
     hardwario.setRelay(RelayState.Off)
 })
 input.onButtonPressed(Button.A, function () {
+    hardwario.setPowerModuleRelay(RelayState.On)
     hardwario.setRelay(RelayState.On)
 })
 
 /**
  * Run all the measurements on all the supported Modules and Tags
+ * If you have a physical hardware from HARDWARIO you will get all the measured values
+ * Othervise everything should be a NaN
  */
 basic.showNumber(hardwario.humidity())
 basic.showNumber(hardwario.co2())
@@ -20,3 +25,11 @@ basic.showNumber(hardwario.altitude())
 basic.showNumber(hardwario.pressure())
 basic.showNumber(hardwario.batteryVoltage(BatteryModuleType.Mini))
 basic.showNumber(hardwario.vocLP())
+
+/**
+ * Sets up the motion detector on PIR Module and on movement there should be a hearth icon on the LED matrix
+ */
+hardwario.motionDetectorTask()
+hardwario.onMovement(function () {
+    basic.showIcon(IconNames.Heart);
+})
